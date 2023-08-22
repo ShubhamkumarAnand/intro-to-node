24209cca661c855209b6f8ba64b70ea6f9384f6c
imskanand <the.anand3099@gmail.com>
2023-08-23 00:25:45 +0530

    Added yargs lib and helpful commands to the app

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
-import fs from 'node:fs'
 
-const note = process.argv[2];
-
-const newNote = {
-  note,
-  id: Date.now(),
-};
-
-console.log(newNote);
+import "./src/command.js";
